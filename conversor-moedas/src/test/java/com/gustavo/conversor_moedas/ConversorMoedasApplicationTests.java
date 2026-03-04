e302e51313131cfeef2a56bc56d1d11188c34425
Gustavo Manoel Elias <gustavomanoelelias13@gmail.com>
2026-03-04 15:00:46 -0300

    feat: estrutura inicial da API de conversão de moedas

---
+++ conversor-moedas/src/test/java/com/gustavo/conversor_moedas/ConversorMoedasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.conversor_moedas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversorMoedasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
